43114dfd69b499030a3365920816a21e2087e02c
shwibi <shwibishrabs285@gmail.com>
2021-07-03 20:19:33 +0530

    3/7/2021, 8:19:33 pm

---
+++ Update.js
@@ -0,0 +1,52 @@
+const { exec } = require("child_process");
+
+exec(`node reader.js -r -d -p`, (error, stdout, stderr) => {
+	if (error) {
+		console.log(`error: ${error.message}`);
+		return;
+	}
+	if (stderr) {
+		console.log(`stderr: ${stderr}`);
+		return;
+	}
+	console.log(`stdout: ${stdout}`);
+	console.log("Yoo!");
+
+	exec(`git add .`, (error, stdout, stderr) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+		}
+		console.log(`stdout: ${stdout}`);
+		console.log("Yoo!");
+
+		exec(
+			`git commit -m "${new Date().toLocaleString()}"`,
+			(error, stdout, stderr) => {
+				if (error) {
+					console.log(`error: ${error.message}`);
+					return;
+				}
+				if (stderr) {
+					console.log(`stderr: ${stderr}`);
+				}
+				console.log(`stdout: ${stdout}`);
+				console.log("Yoo!");
+				exec(`git push`, (error, stdout, stderr) => {
+					if (error) {
+						console.log(`error: ${error.message}`);
+						return;
+					}
+					if (stderr) {
+						console.log(`stderr: ${stderr}`);
+					}
+					console.log(`stdout: ${stdout}`);
+					console.log("Yoo!");
+				});
+			}
+		);
+	});
+});
